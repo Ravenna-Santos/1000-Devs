8cc5a9aa0bdb23df1466b65f0a79bebafd7190b6
Ravenna-Santos <ravennasantos2016@gmail.com>
2023-11-19 22:18:24 -0300

    19/11/2023 22:20

--- mesttra/POO - Michelle/exercícios/classes e objetos/agenda.js
+++ mesttra/POO - Michelle/exercícios/classes e objetos/agenda.js
@@ -15,21 +15,32 @@ var agenda = ({
 
     },
 
-    editar(){
-
+    editar(posicao, novoNome, novoSobrenome, novoNumero){
+        arrayAgenda[posicao - 1] = new Contato(novoNome, novoSobrenome, novoNumero)
     },
 
-    excluir(){
+    excluir(posicao){
+        arrayAgenda.splice(posicao - 1, 1)
+    },
 
+    imprimir(){
+        let contato;
+        console.log("id".padEnd(5) + "nome".padEnd(20) + "número".padEnd(10))
+        for(let index = 0; index < arrayAgenda.length; index++){
+            contato = arrayAgenda[index]
+            console.log(String(index).padEnd(5) + String(contato.nome + " " + contato.sobrenome ).padEnd(20) + String(contato.numero).padEnd(10))
+        }
     }
 
 
 })
 
 agenda.adicionar("Ravenna", "Santos", 123)
-console.log(arrayAgenda)
+agenda.imprimir()
 agenda.adicionar("Maria", "Jesus", 123)
-console.log(arrayAgenda)
+agenda.imprimir()
+agenda.excluir(1)
+agenda.imprimir()
 
 
 
