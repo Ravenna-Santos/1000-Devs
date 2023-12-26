da8a90300db000d2bd1e93c8a1c4c93bd245e6d4
Ravenna-Santos <ravennasantos2016@gmail.com>
2023-12-26 12:57:45 -0300

    26/11/2023 12:57

--- mesttra/Node-APIs/pokedex/scripts.js
+++ mesttra/Node-APIs/pokedex/scripts.js
@@ -1,19 +1,25 @@
 const listaPokemonsHtml = document.getElementById("pokemons");
 
-const getPokemons = () => {
+const getPokemons = async (name = '') => {
 
-    const Api = fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
-    console.log(Api);
-    
-    Api.then((response) => {
-        //console.log(response);
-        return response.json()
-    }).then((data) => {
-        render(data.results);
-    })
+    if(name !== ''){
+        
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = await response.json();
+        renderOne(data)
+        
+    }else{
+        
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
+        const data = await response.json();
+        renderAll(data.results)
+
+    }
+
 }
 
-const render = (pokemons) => {
+const renderAll = (pokemons) => {
+    listaPokemonsHtml.innerHTML = '';
     pokemons.map((pokemon, index) => {
         listaPokemonsHtml.insertAdjacentHTML("beforeend", 
         `<li class="pokemon-card">
@@ -21,6 +27,31 @@ const render = (pokemons) => {
             <h3>${pokemon.name}</h3>
         </li>`)
     })
+}
+
+const renderOne = (pokemon) => {
+    console.log(pokemon)
+    listaPokemonsHtml.innerHTML = '';
+        listaPokemonsHtml.insertAdjacentHTML("beforeend", 
+        `<li class="pokemon-card">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.id}.gif"/>
+            <h3>${pokemon.name}</h3>
+            <h4>Peso: ${pokemon.weight}</h4>
+            <p> Tipo:
+            ${pokemon.types.map(tipoPokemon => `
+            ${tipoPokemon.type.name}
+            `).join(' / ')}
+            </p>
+        </li>`)
+
+}
+
+const searchPokemon = (event) => {
+    event.preventDefault();
+    const text = document.getElementById("buscarInput").value;
+    
+    getPokemons(text);
+
 }
 
 getPokemons();
